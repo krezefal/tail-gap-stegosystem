5c73aed862d343ff8c957b9561f48f00b4f6ac8b
Timofey Klimov <klimovtimofeyy@gmail.com>
2021-12-14 23:40:28 +0300

    funcs finalization, program improvement: adaptation to procedure files with carriage return symbols, formatting and cleaning the code

--- cmd/main.go
+++ cmd/main.go
@@ -5,30 +5,34 @@ import (
 	. "github.com/krezefal/tail-gap-stegosystem/pkg"
 )
 
-func main() {
-	fmt.Println("Begin encoding...")
+var srcFileName = "examples/example.html"
+var tgFileName = "examples/message_inside.html"
 
-	srcFile, _ := OpenFile("examples/example.txt")
+func main() {
+	fmt.Println("\nBegin encoding...")
+
+	srcFile, _ := OpenFile(srcFileName)
 	defer srcFile.Close()
 
-	tgFile, _ := CreateFile("examples/message_inside.txt")
+	tgFile, _ := CreateFile(tgFileName)
 	defer tgFile.Close()
 
-	message := GenerateMessage(100)
+	//message := GenerateMessage("10100")
+	message := GenerateRandomMessage(100)
 	fmt.Println("Message:", message)
 	fmt.Println("Begin embed message into file-container...")
 	bitCounter, _ := EmbedMessage(srcFile, message, tgFile)
-	fmt.Println("Message bit number inside file-container:", bitCounter)
+	fmt.Println("Number of message bits inside file-container:", bitCounter)
 	fmt.Println("Message embedding completed.")
 	tgFile.Close()
 
-	fmt.Println("Begin decoding...")
+	fmt.Println("\nBegin decoding...")
 
-	tgFile, _ = OpenFile("examples/message_inside.txt")
+	tgFile, _ = OpenFile(tgFileName)
 
 	fmt.Println("Begin extracting message from file-container...")
 	messageOut, _ := ExtractMessage(tgFile)
 	fmt.Println("Extracted message:", messageOut)
+	fmt.Println("Extracted message length:", len(messageOut))
 	fmt.Println("Message extracting completed.")
 }
-
--- pkg/utils.go
+++ pkg/utils.go
@@ -1,10 +1,12 @@
 package pkg
 
 import (
+	"math/rand"
 	"os"
+	"time"
 )
 
-// OpenFile pkg opens file
+// OpenFile func opens file
 func OpenFile(fileName string) (*os.File, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -13,7 +15,7 @@ func OpenFile(fileName string) (*os.File, error) {
 	return file, nil
 }
 
-// CreateFile pkg creates file
+// CreateFile func creates file
 func CreateFile(fileName string) (*os.File, error) {
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -22,37 +24,61 @@ func CreateFile(fileName string) (*os.File, error) {
 	return file, nil
 }
 
-// GenerateMessage pkg generates either random or specified binary sequence with specified length
-// TODO: random generation mode + specified sequence
-func GenerateMessage(length int) []byte {
-	message := make([]byte, length)
+// GenerateMessage func generates specified binary sequence
+func GenerateMessage(sequence string) []byte {
+	message := make([]byte, len(sequence))
 
-	for i := 0; i < cap(message); i++ {
-		if i % 2 == 1 {
-			message[i] = 0x00
-		} else {
+	for i, bit := range sequence {
+		if bit == '1' {
 			message[i] = 0x01
+		} else {
+			message[i] = 0x00
 		}
 	}
 
 	return message
 }
 
-// PrepareLine pkg prepares container-line by removing all spaces at the end of string and newline symbol
-// this sequence will interfere correct message extracting during the extracting session
-func PrepareLine(line string) string {
+// GenerateRandomMessage func generates random binary sequence with specified length
+func GenerateRandomMessage(length int) []byte {
+	rMessage := make([]byte, length)
 
-	if line == "\n" {
-		return ""
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+
+	for i := 0; i < cap(rMessage); i++ {
+		if r.Int()%2 == 1 {
+			rMessage[i] = 0x00
+		} else {
+			rMessage[i] = 0x01
+		}
 	}
 
-	var cutIndex int
+	return rMessage
+}
 
-	for cutIndex = len(line) - 2; cutIndex >= 0; cutIndex-- {
-		if line[cutIndex] != ' ' {
+// PrepareLine func prepares container-line by removing all spaces at the end of string
+// with newline and carriage return symbols
+// This sequence will interfere correct message extracting during the extracting session
+func PrepareLine(line string) (string, bool) {
+
+	var carriageReturnFlag bool
+
+	if line == "\n" {
+		return "", carriageReturnFlag
+	}
+
+	idx := len(line) - 2
+
+	if line[idx] == '\r' {
+		carriageReturnFlag = true
+		idx--
+	}
+	for ; idx >= 0; idx-- {
+		if line[idx] != ' ' {
 			break
 		}
 	}
 
-	return line[:cutIndex + 1]
+	return line[:idx+1], carriageReturnFlag
 }
