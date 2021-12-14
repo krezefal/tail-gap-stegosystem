d5638dbd4a66c234d25f1911d5201b5c52c384d0
Timofey Klimov <klimovtimofeyy@gmail.com>
2021-12-14 23:40:58 +0300

    test improvement

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -8,32 +8,50 @@ import (
 func TestPrepareLine(t *testing.T) {
 	line1 := "\n"
 	line2 := " \n"
-	line3 := "actually normal string\n"
-	line4 := "actually normal string with space at the end \n"
-	line5 := "actually normal string with 2 spaces at the end  \n"
+	line3 := "\r\n"
+	line4 := " \r\n"
+	line5 := "actually normal string\n"
+	line6 := "actually normal string with space at the end \n"
+	line7 := "actually normal string with carriage return\r\n"
+	line8 := "actually normal string with space at the end and carriage return \r\n"
 
-	result := pkg.PrepareLine(line1)
-	if result != "" {
-		t.Errorf("1. Result was incorrect. Got: |%s|, expected: |%s|", result, "")
+	result, crFlag := pkg.PrepareLine(line1)
+	if result != "" && crFlag != false {
+		t.Errorf("1. Result is incorrect. Got: |%s|, expected: |%s|", result, "")
 	}
 
-	result = pkg.PrepareLine(line2)
-	if result != "" {
-		t.Errorf("2. Result was incorrect. Got: |%s|, expected: |%s|", result, "")
+	result, crFlag = pkg.PrepareLine(line2)
+	if result != "" && crFlag != false {
+		t.Errorf("1. Result is incorrect. Got: |%s|, expected: |%s|", result, "")
 	}
 
-	result = pkg.PrepareLine(line3)
-	if result != "actually normal string" {
-		t.Errorf("3. Result was incorrect. Got: |%s|, expected: |%s|", result, "actually normal string")
+	result, crFlag = pkg.PrepareLine(line3)
+	if result != "" && crFlag != true {
+		t.Errorf("3. Result is incorrect. Got: |%s|, expected: |%s|", result, "")
 	}
 
-	result = pkg.PrepareLine(line4)
-	if result != "actually normal string with space at the end" {
-		t.Errorf("4. Result was incorrect. Got: |%s|, expected: |%s|", result, "actually normal string with space at the end")
+	result, crFlag = pkg.PrepareLine(line4)
+	if result != "actually normal string" && crFlag != true {
+		t.Errorf("4. Result is incorrect. Got: |%s|, expected: |%s|", result, "")
 	}
 
-	result = pkg.PrepareLine(line5)
-	if result != "actually normal string with 2 spaces at the end" {
-		t.Errorf("5. Result was incorrect. Got: |%s|, expected: |%s|", result, "actually normal string with 2 spaces at the end")
+	result, crFlag = pkg.PrepareLine(line5)
+	if result != "actually normal string" && crFlag != false {
+		t.Errorf("5. Result is incorrect. Got: |%s|, expected: |%s|", result, "actually normal string")
+	}
+
+	result, crFlag = pkg.PrepareLine(line6)
+	if result != "actually normal string with space at the end" && crFlag != false {
+		t.Errorf("6. Result is incorrect. Got: |%s|, expected: |%s|", result, "actually normal string with space at the end")
+	}
+
+	result, crFlag = pkg.PrepareLine(line7)
+	if result != "actually normal string with carriage return" && crFlag != true {
+		t.Errorf("7. Result is incorrect. Got: |%s|, expected: |%s|", result, "actually normal string with carriage return")
+	}
+
+	result, crFlag = pkg.PrepareLine(line8)
+	if result != "actually normal string with space at the end and carriage return" && crFlag != true {
+		t.Errorf("8. Result is incorrect. Got: |%s|, expected: |%s|", result, "actually normal string with space at the end and carriage return")
 	}
 }
